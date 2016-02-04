4f588dcf28437ef76a634ad67853a5bf5b60399f
matosb2 <justmats@gmail.com>
2016-02-03 19:14:08 -0500

    Remove messages and comment in understanding of code
    Remove messages because was not instructed to include them and went
    really in-depth with understanding of code throughout file

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ var Enemy = function(initialX, initialY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.x = initialX;
     this.y = initialY;
     this.speed = speed;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -23,13 +23,17 @@ Enemy.prototype.update = function(dt) {
         this.randomSpeed();
     }
 
+    //This is the range in which the player can get to the enemy 
+    //and vice versa before the collision happens
     var bugXLeftRange = this.x - 50;
     var bugXRightRange = this.x + 50;
     var bugYTopRange = this.y - 50;
     var bugYBottomRange = this.y + 50;
-
+    //Handles the collision between player and enemy here so if 
+    //player gets in range within 50 of enemy, the player 
+    //will be reset back to it's starting point due to the
+    //resetPlayerPosition function being invoked.
     if (player.x > bugXLeftRange && player.x < bugXRightRange && player.y > bugYTopRange && player.y < bugYBottomRange) {
-        enemyBugMessage();
         player.resetPlayerPosition();
     }
 };
@@ -38,50 +42,69 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
+//Random method is used in this function to get a number from 1-5 to 
+//multiply by 75 to generate a random speed to use for dt
 Enemy.prototype.randomSpeed = function() {
     var speedMultiply = Math.floor(Math.random() * 5 + 1);
     this.speed = 75 * speedMultiply;
 };
 
-
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+//Player will appear at this default location on x and y axis of canvas
 var playerInitialX = 200,
     playerInitialY = 400;
 //*Create player class
 var Player = function() {
     this.x = playerInitialX;
     this.y = playerInitialY;
+    //Sets wallChecker values to default.  Left and Right walls 
+    //are false because player is not next to a left or right wall.
+    //Bottom wall is true because player spawns at bottom middle of canvas
+    //near a bottomWall
     this.wallChecker = {
         leftWall: false,
         rightWall: false,
         bottomWall: true
     };
+    //Player's image is loaded
     this.sprite = 'images/char-boy.png';
 };
 
-
+//Player's update method uses dt as a parameter just like 
+//enemy's update method so everything can run at same speed
+//across all systems running the game
 Player.prototype.update = function(dt) {
-
 };
 
+//Player's image is rendered on canvas at it's
+//x and y location mentioned above
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//Resets player back to default location and then 
+//resetCheckPosition becomes invoked from below
 Player.prototype.resetPlayerPosition = function() {
     this.x = playerInitialX;
     this.y = playerInitialY;
     this.resetCheckPosition();
 };
 
+//Player movement is set to move by the push of the keys 
+//listed below (left, right, up, down).  Player moves a 
+//length of 90 up or down, and a length of 100 left or right
+//across the canvas.  CheckPosition is invoked to find out if 
+//Player is near a wall.
 Player.prototype.handleInput = function(keyPressed) {
     var stepHorizontalLength = 100,
         stepVerticalLength = 90;
     this.CheckPosition();
+
+    //When left key is pressed move left 100
+    //on x axis of canvas until wall reached
     if (keyPressed === 'left') {
         if (this.wallChecker.leftWall) {
             return null;
@@ -89,6 +112,8 @@ Player.prototype.handleInput = function(keyPressed) {
 
         this.x -= stepHorizontalLength;
 
+    //When right key is pressed move right 100
+    //on x axis of canvas until wall reached
     } else if (keyPressed === 'right') {
         if (this.wallChecker.rightWall) {
             return null;
@@ -96,27 +121,40 @@ Player.prototype.handleInput = function(keyPressed) {
 
         this.x += stepHorizontalLength;
 
+    //When up key is pressed move up 90
+    //on y axis of canvas until you reach 40
+    //on y axis and then reset player position
+    //(Thats how you beat the game)
     } else if (keyPressed === 'up') {
         if (this.y === 40) {
-            playerWinMessage();
             this.resetPlayerPosition();
             return null;
         }
 
         this.y -= stepVerticalLength;
 
+    //When down key is pressed move down 90
+    //on y axis of canvas until you reach a wall
     } else if (keyPressed === 'down') {
         if (this.wallChecker.bottomWall) {
             return null;
         }
 
         this.y += stepVerticalLength;
+
+    //if any other key but up, down, left, right
+    //are pushed return this message on console
     } else {
         console.log('>>> WRONG KEY PRESSED <<<');
         return null;
     }
 };
 
+//If player is at 0 on x axis then he is near a left wall
+//but not a right wall.  If a player is at 400 on x axis vice versa.
+//If he is anywhere else on x axis he is not near any walls.
+//If player is at 400 on the y axis he is near a bottom wall
+//otherwise he is not near a wall.
 Player.prototype.CheckPosition = function() {
     if (this.x === 0) {
         this.setHorizontalWallCheckerState(true, false);
@@ -133,11 +171,18 @@ Player.prototype.CheckPosition = function() {
     }
 };
 
+//Sets wall checkers back to default value considering player
+//will be back to default location
 Player.prototype.resetCheckPosition = function() {
     this.setHorizontalWallCheckerState(false, false);
     this.wallChecker.bottomWall = true;
 };
 
+//This function when invoked up top changes the values of
+//left and rightwallstate to either true or false which is why
+//they are parameters.  If either of these are true then it just
+//means that player is near whichever wall that lists true not
+//near a wall if it's false.
 Player.prototype.setHorizontalWallCheckerState = function(leftWallState, rightWallState) {
     this.wallChecker.leftWall = leftWallState;
     this.wallChecker.rightWall = rightWallState;
@@ -148,6 +193,13 @@ Player.prototype.setHorizontalWallCheckerState = function(leftWallState, rightWa
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
 
+//Created a copy of Enemy class so we could set the individual 
+//locations of each enemy (3 of them) on the x and y axis through
+//this for loop and also set a random speed to each one.
+//Looks confusing but the -60...tempSpeed are just substituting the 
+//parameters from the enemy object.  So -60 is where
+//these enemies will start on x axis, 60+85*i is where they start
+//on y axis going as fast as tempSpeed allows them across the canvas.
 for (var i = 0; i < 3; i++) {
     var tempSpeed = Math.floor(Math.random() * 5 + 1) * 75;
     allEnemies.push(new Enemy(-60, 60 + 85 * i, tempSpeed));
@@ -168,12 +220,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
-var enemyBugMessage = function() {
-    console.log('>>> NOM NOM NOM START OVER PEASANT');
-};
-
-var playerWinMessage = function () {
-    console.log('CONGRATULATIONS YOU WIN!!')
-}
